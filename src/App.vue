7cf708b4e2442bf6bbe2cfee7097ed35155608a3
Igor Amidzic <amidzicigor@yahoo.com>
2017-06-29 22:44:06 -0400

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <app-navbar></app-navbar>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  components: {
+    appNavbar: Navbar
+  }
+}
+</script>
+
+<style lang="sass">
+@import 'src/public/sass/_variables.sass'
+@import 'src/public/sass/main.sass'
+</style>
