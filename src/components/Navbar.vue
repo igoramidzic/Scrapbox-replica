32b94250fdd1d51a39e71010efd633143daa5958
Igor Amidzic <amidzicigor@yahoo.com>
2017-07-03 22:45:52 -0400

    Update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <nav class="main-navbar">
+  <nav class="main-navbar" @click="menuToggle">
     <div class="container-fluid">
       <div class="row">
         <!-- Logo -->
@@ -22,7 +22,33 @@
         <!-- Menu -->
         <div class="nav-section nav-section-menu col-md-3 col-sm-4 col-xs-4">
           <div class="holder">
-            <button class="nav-menu-btn" type="button" name="button"><span class="hidden-xs">Menu</span> <span class="fa fa-bars"></span></button>
+            <button class="nav-menu-btn" type="button" name="button" @click="menuBinary = !menuBinary"><span class="hidden-xs">Menu</span> <span class="fa fa-bars"></span></button>
+            <div class="nav-menu-dropdown" v-if="menuBinary">
+              <ul class="list">
+                <li><a class="nav-link emphasis nav-item" href="#">Igor Amidzic's project</a></li>
+                <li><a class="nav-link nav-item" href="#">Create new project +</a></li>
+                <li class="divider"></li>
+                <li class="section-header nav-item">Igor Amidzic's project</li>
+                <li><a class="nav-link nav-item" href="#">Members (1)</a></li>
+                <li><a class="nav-link nav-item" href="#">Settings</a></li>
+                <li><a class="nav-link nav-item" href="#">Theme</a></li>
+                <li><a class="nav-link nav-item" href="#">Notifications</a></li>
+                <li class="divider"></li>
+                <li class="section-header nav-item">Style</li>
+                <li><a class="nav-link emphasis nav-item" href="#">Grid</a></li>
+                <li><a class="nav-link nav-item" href="#">List</a></li>
+                <li class="divider"></li>
+                <li class="section-header nav-item">Igor Amidzic</li>
+                <li><a class="nav-link nav-item" href="#">Edit profile</a></li>
+                <li><a class="nav-link nav-item" href="#">Log out</a></li>
+                <li class="divider"></li>
+                <li><a class="nav-link nav-item" href="#">About Scrapbox</a></li>
+                <li><a class="nav-link nav-item" href="#">Help</a></li>
+                <li class="divider"></li>
+                <li class="section-header nav-item">Watch List</li>
+                <li><a class="nav-link nav-item" href="#">Scrapbox Help</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -32,7 +58,20 @@
 
 <script>
 export default {
+  data: () => {
+    return {
+      menuBinary: false
+    }
+  },
+  methods: {
+    menuToggle: () => {
+      if (this.menuBinary) {
+        this.menuBinary = false
+      }
+    }
+  }
 }
+
 </script>
 
 <style lang="sass" scoped>
@@ -96,6 +135,38 @@ export default {
               margin-left: 10px
           &:hover
             color: rgb(228, 228, 228)
-
+          .nav-menu-dropdown
+            background-color: white
+            color: $primary-text-color
+            position: absolute
+            right: 0
+            width: 300px
+            border-radius: 4px
+            border: 1px solid rgba(0,0,0,0.15)
+            box-shadow: 0 6px 12px rgba(0,0,0,0.175)
+            -webkit-box-shadow: 0 6px 12px rgba(0,0,0,0.175)
+            .list
+              text-align: left
+              list-style-type: none
+              padding: 5px 0px
+              margin: 0px
+              .nav-item
+                padding: 3px 20px
+              .divider
+                height: 1px
+                width: 100%
+                background-color: #e5e5e5
+                margin: 9px 0px
+              .emphasis
+                color: $primary-color1 !important
+              .nav-link
+                color: $primary-text-color
+                display: block
+                text-decoration: none
+                &:hover
+                  background-color: #f5f5f5
+              .section-header
+                color: #777
+                font-size: 13px
 
 </style>
